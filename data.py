888034dfdb6305f34c501a4f538d817c5b571abc
Yuval Adam <yuv.adm@gmail.com>
2012-12-24 13:57:57 +0200

    Initial csv data parsing

---
+++ data.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+
+city_data = []
+criteria_data = []
+
+with open('data.csv', 'r') as f:
+    data = [x for x in f]
+
+    categories = [cat for cat in data[0].split(',') if cat]
+    criteria = data[-1].split(',')[2:-6]
+
+    criteria_counts = [23, 3, 16, 15, 6, 6, 13, 11, 2, 8, 20, 5]
+
+    for row in data[2:-2]:
+        cells = row.split(',')
+        website = cells[0]
+        city = cells[1]
+        values = cells[2:-6]
+
+        row_data = []
+
+        count = 0
+        for c in criteria_counts:
+            row_data.append(values[count:count + c])
+            count += c
+
+        city_data.append({
+            'name': city,
+            'website': website,
+            'data': row_data
+        })
+
+pprint(city_data)
